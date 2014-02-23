3c8a3bb797c5e171d524f27343406b24f0f5737d
Steven <theepicsnail@gmail.com>
2014-02-22 20:49:40 -0500

    Refactoring and prep for throttling

--- paest_server/paest.py
+++ paest_server/paest.py
@@ -62,20 +62,29 @@ class PaestServer(RequestHandler):
                         "http://pae.st/{PID}#WEB-PUBLIC\n"
                         "http://pae.st/{PID}/{KEY}#WEB-PRIVATE\n"
                         ).format(PID=p_id, KEY=p_key))
-def main():
-    """ Define routing, and start the server """
-    # Setup Tornado
-    define("tornado_port", default=80, help="Port to host web server on")
-    define("backend", default="redis", help="Which backend to use")
+
+def setup_options():
+    """ Set up all of the flags for paest (and possible backends) """
+    # Various engines in paest. 
+    define("backend", default="redis", help="Which backend to use", 
+        group="paest")
+    define("throttler", default="redis", help="Which throttler to use", 
+        group="paest")
+
+    # Setup Tornado Server options
+    define("tornado_port", default=80, help="Port to host web server on", 
+            group="tornado")
    
-    # Redis
+    # Redis options
     define("redis_host", default="localhost", help="Redis server host", 
             group="redis")
     define("redis_port", default=6379, help="Redis server port", group="redis")
     define("redis_db", default=0, help="Redis DB to use", group="redis")
 
     options.parse_command_line()
-    
+ 
+def get_db():
+    """ Get (and configure) a paest db backend """
     def use_redis():
         """ Configure and return a RedisDB instance """
         # pylint reimport warning bug
@@ -84,13 +93,37 @@ def main():
         return RedisDB(host=options.redis_host,
                        port=options.redis_port, 
                        db=options.redis_db)
-    paestdb = {
-        "redis":use_redis
-    }.get(options.backend)()
+    # def use_someOtherDB():
+    #   import your code
+    #   create your client
     
+    return {
+        "redis":use_redis
+        # Add your backend flag here. (Add your options in setup_options)
+    }.get(options.backend, lambda:None)()
+
+def get_throttler():
+    """ Get a throttling backend """
+    def use_redis():
+        """ Configure and return a RedisThrottler """
+        return None # TODO(snail): Implement throttling backend
+
+    return {
+        "redis":use_redis
+    }.get(options.throttler, lambda :None)()
+
+def main():
+    """ Define routing, and start the server """
+    setup_options()   
+    paestdb = get_db()
+  
     if paestdb is None:
         print "Backend unavailable. Exiting."
         exit(1)
+
+    throttler = get_throttler()
+    if throttler is None:
+        print "Throttling unavailable. Continuing"
 
 
     # Build the matcher. (<paest id> and <paest key> are base58 strings)
