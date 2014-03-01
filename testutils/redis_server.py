73702c3527247c42d05f5874c86e621f5fb31df8
Steven <theepicsnail@gmail.com>
2014-03-01 00:04:48 -0500

    Attempting to fix redis-server path problems

--- testutils/redis_server.py
+++ testutils/redis_server.py
@@ -4,10 +4,10 @@ This starts an actual redis server with a test configuration
 import subprocess
 import time
 
-TEST_REDIS_BIN = "/usr/bin/redis-server"
-
+# Find redis-server binary. There should be a better way to do this.
+TEST_REDIS_BIN = subprocess.check_output(["locate", "/redis-server"]).strip()
 class RedisServer(object):
-    def __init__(self):
+    def __init__(self, port):
         self.process = subprocess.Popen([TEST_REDIS_BIN, "--port", str(port)])
         time.sleep(.1)
 
