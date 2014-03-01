3c4e1cd9f51102729744104994139c40dee408d1
Steven <theepicsnail@gmail.com>
2014-02-28 22:44:04 -0500

    Attempting to figure out where travis-ci installs redis-server

--- testutils/redis_server.py
+++ testutils/redis_server.py
@@ -3,10 +3,12 @@ This starts an actual redis server with a test configuration
 """
 import subprocess
 import time
-_redis_bin = "/usr/sbin/redis-server"
+
 class RedisServer(object):
     def __init__(self, port):
-        self.process = subprocess.Popen([_redis_bin, "--port", str(port)])
+        subprocess.Popen(['which', 'redis-server'])
+        subprocess.Popen(['locate', 'redis-server'])
+        self.process = subprocess.Popen(["redis-server", "--port", str(port)])
         time.sleep(.1)
 
     def stop(self):
