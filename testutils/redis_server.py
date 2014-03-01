d9759c30427e5034a3178adccc08daa93419d8c0
Steven <theepicsnail@gmail.com>
2014-02-28 22:52:54 -0500

    Found redis-server. Fixing reference.

--- testutils/redis_server.py
+++ testutils/redis_server.py
@@ -4,11 +4,11 @@ This starts an actual redis server with a test configuration
 import subprocess
 import time
 
+TEST_REDIS_BIN = "/usr/bin/redis-server"
+
 class RedisServer(object):
-    def __init__(self, port):
-        subprocess.Popen(['which', 'redis-server'])
-        subprocess.Popen(['locate', 'redis-server'])
-        self.process = subprocess.Popen(["redis-server", "--port", str(port)])
+    def __init__(self):
+        self.process = subprocess.Popen([TEST_REDIS_BIN, "--port", str(port)])
         time.sleep(.1)
 
     def stop(self):
