61a982011800b3d291198facdd4f455760ce6d91
Steven <theepicsnail@gmail.com>
2014-03-08 14:16:48 -0500

    Updated throttling documentation

--- paest_server/throttling/redisimpl.py
+++ paest_server/throttling/redisimpl.py
@@ -7,15 +7,36 @@ from . import Throttler
 class RedisThrottler(Throttler):
     """ A PaestThrottler implemented in redis """
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self, actions=10, window=10, *args, **kwargs):
+        """ create a throttler that uses redis during throttling.
+        actions - the number of actions allowed during a given window of time
+        window - the number of seconds to keep track of actions
+
+        This throttler may allow up to 2*actions in the width of a single window
+        of time. This occurs when actions cross the window boundary.
+
+        For example window=3 seconds, actions = 2 (2 actions per 3 seconds)
+        :00 -- window starts --
+        :01
+        :02 action
+            action
+        :03 -- window starts --
+            action
+            action
+        :04
+        :05
+
+            4 actions occured during the 2 second window from :02 to :04
+            This is permittable as the average over those two windows is still
+            only 2 actions per 3 seconds.
+        """
         super(RedisThrottler, self).__init__()
-        # 10 interactions per 10 seconds
-        self.max_interactions = 10
-        self.window_size = 10
+        self.max_interactions = actions
+        self.window_size = window
         self.client = redis.StrictRedis(*args, **kwargs)
 
     def reject(self, request_uid):
-        """ asf"""
+        """ Returns True if the request should be rejected """
         uid = request_uid + str(int(time.time()/self.window_size))
 
         redis_key = "throttler:{}".format(hash(uid))
