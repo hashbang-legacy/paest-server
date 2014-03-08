35d80ab53485a82c5458dbf28b09dcb03dc23dca
Steven <theepicsnail@gmail.com>
2014-03-08 04:07:41 -0500

    Added supervisord

--- run.sh
+++ run.sh
@@ -5,12 +5,15 @@
 #To launch the server:
 #./run <args>
 
-if [ "$1" == "test" ]
-then
-shift
-echo "Running tests"
-python -m unittest discover $@
+if [ "$1" == "test" ] ; then
+  shift
+  echo "Running tests"
+  python -m unittest discover $@
+else if [ "$1" == "supervisord" ] ; then
+  supervisord -c supervisor/supervisord.conf
+else if [ "$1" == "supervisorctl" ] ; then
+  supervisorctl -c supervisor/supervisord.conf
 else
-echo "Launching paest."
-python paest_server/paest.py $@
-fi
+  echo "Launching paest."
+  python paest_server/paest.py $@
+fi fi fi
